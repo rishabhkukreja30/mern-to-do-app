9a9f77c5609be77d52092fa2148b852a038972e9
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-02-15 16:01:38 +0530

    added get todos on frontend

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const userRouter = require("./routes/user");
 const todosRouter = require("./routes/todos");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.use("/users", userRouter);
 app.use("/todos", todosRouter);
 
--- frontend/todo frontend/src/App.jsx
+++ frontend/todo frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import CreateTodo from "./components/CreateTodo";
+import Todos from "./components/Todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
+  let x = true;
+
+  if (x) {
+    fetch("http://localhost:3000/todos").then(async (res) => {
+      const json = await res.json();
+      console.log(json.todos);
+      setTodos(json.todos);
+    });
+    x = false;
+  }
+
   return (
     <div>
       <CreateTodo />
+      <Todos todos={todos} />
     </div>
   );
 }
